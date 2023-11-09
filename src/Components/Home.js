8fa2f75ffea8d3328709aae16a8c3061f7883463
Lalakiya Priyanka <priyankalalkiya192@gmail.com>
2023-11-09 16:44:41 +0530

    added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,37 @@
 import React from "react";
 import img from './img.gif';
+import backgroundImg from './bg1.avif'; // Import the background image
 
 const Home = () => {
+  // Define the style for the background image
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImg})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
+
   return (
-    <section className="home">
+    <section className="home" style={backgroundStyle}>
       <div className="main">
         <div className="box">
           <div className="text">
             <h3 className="heygreet">Hey, I am </h3>
             <h1 data-text="Priyanka" className="name project-title-text">Priyanka Lalakiya </h1>
-          
             <h3 className="animation">
               <span></span>
             </h3>
+            <div  className="content-text">
+            <h4>Starting the process of developing an API-driven React website opens up a world of infinite possibilities for improving your website's functionality and providing users with real-time data and information.",
+        demo:"url.com</h4></div>
+            <button className="resume-btn" >Downlode Resume <i class="fa-solid fa-download"></i></button>
           </div>
         </div>
         <div className="box1">
-        <div className="image">
-          <img src={img} alt="" />
+          <div className="image">
+            <img src={img} alt="" />
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
-import logo from './1logo.jpg'
+import logo from './1logo.jpg';
+
 const Navbar = () => {
-  const [clicked, setclicked] = useState(false)
+  const [clicked, setClicked] = useState(false);
 
-  const handleClick = ()=>{
-    setclicked(!clicked);
-  }
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <> 
-   <nav>
-   < div>
-   <img  className='circle' src={logo} alt="My Logo" />
-   </div>
-    <div>
-        <ul id='navbar' className={clicked ? '#navbar active':''}>
-            <li><a className='active' href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Project</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">Contact</a></li>
+    <nav className='sticky-nav'>
+      <div>
+        <div className='logo-container'>
+          <img className='circle' src={logo} alt='My Logo' />
+          <h2>Web Developer</h2>
+        </div>
+      </div>
+      <div>
+        <ul id='navbar' className={clicked ? 'active' : ''}>
+          <li><a className='active' href='#'>Home</a></li>
+          <li><a href='#'>About</a></li>
+          <li><a href='#'>Project</a></li>
+          <li><a href='#'>Skills</a></li>
+          <li><a href='#'>Contact</a></li>
         </ul>
-    </div>
-    <div id="mobile" onClick={handleClick}>
-      <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-   </div>
+      </div>
+      <div id='mobile' onClick={handleClick}>
+        <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
     </nav>
-    </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
