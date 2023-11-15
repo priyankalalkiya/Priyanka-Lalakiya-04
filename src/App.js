c35adc4a513e196f0da4a7cb9c57ae374ad78445
Lalakiya Priyanka <priyankalalkiya192@gmail.com>
2023-11-15 15:21:30 +0530

    added project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-    
+
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import ProjectCard from './Components/ProjectCard';
 import About from './Components/About';
-
+import Skills from './Components/Skills';
+import { skills } from './Components/SkillsData';
 
 function App() {
   return (
-   <>
-    <Navbar/>
-    <Home/>
-    <About/>
-    <ProjectCard/>
-    
-</>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <ProjectCard />
+      <Skills skills={skills} /> {/* Pass the skillsData as a prop to Skills component */}
+    </>
   );
 }
 
