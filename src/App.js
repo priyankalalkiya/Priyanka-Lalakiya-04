57f10b9772caa6a5893813f637ce05b6284355bc
MannMendapara <109096692+MannMendapara@users.noreply.github.com>
2023-12-21 21:39:18 +0530

    Modified footer and contact us page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
-import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import ProjectCard from './Components/ProjectCard';
-import About from './Components/About';
-import Skills from './Components/Skills';
-import { skills } from './Components/SkillsData';
-import Contectline from './Components/Contectline';
-import Contact from './Components/Contact';  // Fix the typo in the filename
-import Footer from './Components/Footer';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import ProjectCard from "./Components/ProjectCard";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import { skills } from "./Components/SkillsData";
+import Contectline from "./Components/Contectline";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -17,11 +15,11 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      <Skills skills={skills} /> 
-      <Contectline/>
+      <Skills skills={skills} />
+      <Contectline />
       <ProjectCard />
- <Contact/>
-     <Footer/>
+      <Contact />
+      <Footer />
     </>
   );
 }
